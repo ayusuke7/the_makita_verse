7c01b5d3b6fbfff919c5cbad20b80ab72fa80975
Alexandre H <alexhcb.7392@gmail.com>
2026-04-10 01:10:33 -0300

    chore: update scripts for dump

--- scripts/main.py
+++ scripts/main.py
@@ -9,16 +9,71 @@ tmc = TheMakitaChronicles()
 yt_dlp = YtDLP()
 
 
+def load_json(filename):
+    with open(filename, "r", encoding="utf-8") as f:
+        return json.load(f)
+
+
 def save_to_json(data, filename):
     with open(filename, "w", encoding="utf-8") as f:
         json.dump(data, f, indent=4, ensure_ascii=False)
 
 
-def update_json(data, filename):
-    with open(filename, "r", encoding="utf-8") as f:
-        old_data = json.load(f)
-        old_data.append(data)
-    save_to_json(old_data, filename)
+def execute_articles(target):
+    all_articles = tmc.get_articles()
+
+    for art in all_articles:
+        news = tmc.get_news_from_article(art)
+
+        if not news:
+            continue
+
+        art["news"] = news
+        filename = f'{art["title"].replace(" ", "_")}.json'
+        save_to_json(art, f'data/{target}/{filename.lower()}')
+        print(f"Arquivo {filename} gerado com sucesso.")
+
+        art.pop("news")
+
+        index_file = f'data/{target}/index.json'
+        index_json = load_json(index_file)
+
+        for a in index_json:
+            if a["url"] == art["url"]:
+                break
+        else:
+            index_json.insert(0, art)
+            save_to_json(index_json, index_file)
+
+
+def execute_podcasts(target):
+    all_podcasts = tmc.get_podcasts()
+
+    for podcast in all_podcasts:
+        podcast = tmc.get_comments_from_podcast(podcast)
+        filename = f'data/{target}/{podcast["title"].replace(" ", "_")}.json'
+        index_file = f'data/{target}/index.json'
+
+        save_to_json(podcast, filename.lower())
+        print(f"Arquivo {filename} gerado com sucesso.")
+
+        podcast.pop("comments")
+        index_json = load_json(index_file)
+
+        for p in index_json:
+            if p["url"] == podcast["url"]:
+                break
+        else:
+            index_json.append(podcast)
+            save_to_json(index_json, index_file)
+
+
+def execute_videos():
+    videos = yt_dlp.get_videos_from_channel("@Akitando")
+    save_to_json(videos, f'data/channel/videos.json')
+
+    playlists = yt_dlp.get_playlists_from_channel("@Akitando")
+    save_to_json(playlists, f'data/channel/playlists.json')
 
 
 def main():
@@ -29,42 +84,13 @@ def main():
     args = parser.parse_args()
 
     if args.target == 'articles':
-        all_articles = tmc.get_articles()
-
-        for art in all_articles:
-            news = tmc.get_news_from_article(art)
-
-            if not news:
-                continue
-
-            art["news"] = news
-            filename = f'data/{args.target}/{art["title"].replace(" ", "_")}.json'
-            save_to_json(art, filename.lower())
-            print(f"Arquivo {filename} gerado com sucesso.")
-
-            art.pop("news")
-            update_json(art, f'data/{args.target}/index.json')
+        execute_articles(args.target)
 
     elif args.target == 'podcasts':
-        all_podcasts = tmc.get_podcasts()
-
-        for podcast in all_podcasts:
-            podcast = tmc.get_comments_from_podcast(podcast)
-            filename = f'data/{args.target}/{podcast["title"].replace(" ", "_")}.json'
-            index = f'data/{args.target}/index.json'
-
-            save_to_json(podcast, filename.lower())
-            print(f"Arquivo {filename} gerado com sucesso.")
-
-            podcast.pop("comments")
-            update_json(podcast, index)
+        execute_podcasts(args.target)
 
     elif args.target == 'videos':
-        videos = yt_dlp.get_videos_from_channel("@Akitando")
-        save_to_json(videos, f'data/channel/videos.json')
-
-        playlists = yt_dlp.get_playlists_from_channel("@Akitando")
-        save_to_json(playlists, f'data/channel/playlists.json')
+        execute_videos()
 
 
 if __name__ == "__main__":
